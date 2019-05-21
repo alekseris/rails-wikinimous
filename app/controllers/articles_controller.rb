7102fc8570d6a863011566b64ff379dd34485e5e
Alex R <jpalre@outlook.com>
2019-05-21 17:30:13 +0900

    done

---
+++ app/controllers/articles_controller.rb
@@ -0,0 +1,20 @@
+class ArticlesController < ApplicationController
+  before_action :set_article, only: [:show, :edit, :update, :destroy]
+
+  def index
+    @articles = Article.all
+  end
+
+  def show
+  end
+
+  def new
+    @article = Article.new
+  end
+
+  private
+
+  def set_article
+    @article = Article.find(params[:id])
+  end
+end
